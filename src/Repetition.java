f69d4c54fd756233dbd2dc32f429078e4872dd86
Stuti Jain <41702974+JainStuti25@users.noreply.github.com>
2020-10-16 18:47:04 +0530

    Update Repetition.java

--- src/Repetition.java
+++ src/Repetition.java
@@ -1,13 +1,28 @@
 import java.util.Scanner;
-
+ 
 public class Repetition {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a=sc.next();
-
-        int max=0;
-        int count =0;
-
-
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        int count = 1;
+        int max = 1;
+        char[] arr = str.toCharArray();
+        for (int i = 1; i < arr.length; ++i) {
+            if (arr[i]==arr[i-1])
+            {
+                count++;
+            }
+            else
+            {
+                if (count>max)
+                    max=count;
+                count=1;
+            }
+        }
+        if (count>max)
+            max=count;
+        System.out.println(max);
+ 
+ 
     }
 }
